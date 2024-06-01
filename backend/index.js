c478a3e0da555be0fafc2a8fd1e43b709e536828
Shivam Kumar <sshivamkumar177@gmail.com>
2024-06-01 12:55:33 +0530

    add new feature

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js";
 import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 
 // Load environment variables from .env file
 dotenv.config({});
@@ -12,14 +12,18 @@ dotenv.config({});
 const app = express();
 
 // Access the PORT variable from the environment
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 
 // middleware
-
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
-
+const corsOption={
+    origin: 'http://localhost:3000',
+    credentials:true
+}
+app.use(cors(corsOption));
 //routes
 
 app.use("/api/v1/user", userRoute);
